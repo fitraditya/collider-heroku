1eeabe572c3beb43c409a636bb46f556cc2a056a
Fitra Aditya <fitra@g.pl>
2015-10-30 10:15:09 +0700

    Initial commit

---
+++ cmd/collider-heroku/main.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
+package main
+
+import (
+	"collider-heroku/Godeps/_workspace/src/collider"
+	"flag"
+	"log"
+	"os"
+)
+
+var envPort = os.Getenv("PORT")
+
+var tls = flag.Bool("tls", false, "Whether TLS is used")
+var port = flag.String("port", envPort, "The TCP port that the server listens on")
+var roomSrv = flag.String("room-server", "https://apprtc.appspot.com", "The origin of the room server")
+
+func main() {
+	flag.Parse()
+
+	log.Printf("Starting collider: tls = %t, port = %d, room-server=%s", *tls, *port, *roomSrv)
+
+	c := collider.NewCollider(*roomSrv)
+	c.Run(*port, *tls)
+}
